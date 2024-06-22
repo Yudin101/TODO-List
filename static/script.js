1445b9bd3f3b987d866832865aa15788ae5aa066
Yudin Karki <yudinkarki00@gmail.com>
2024-06-22 17:19:46 +0545

    removed popups in login/register

--- static/script.js
+++ static/script.js
@@ -2,51 +2,23 @@ let label = document.querySelectorAll('label').forEach(label =>{
 	label.innerHTML = label.innerText.split('').map((letter, i) => `<span style="transition-delay: ${i * 50}ms">${letter}</span>`).join('');
 });
 
-let special = document.getElementById('special-char');
-let minimum = document.getElementById('minimum-char');
-let same = document.getElementById('same-char');
-let popupBg = document.getElementById('popupBg');
-
-
-function openPopupBg() {
-	popupBg.classList.add("open-popupBg");
-}
-
-function closePopup(){
-	special.classList.remove("open-popup");
-	minimum.classList.remove("open-popup");
-	same.classList.remove("open-popup");
-	popupBg.classList.remove("open-popupBg");
-}
-
-function closePopupAlready(){
-	let already = document.getElementById('already-user');
-	let popupBgSecond = document.getElementById('popupBgSecond');
-
-    already.style.visibility = 'hidden';
-    popupBgSecond.style.visibility = 'hidden';
-}
-
 function validateForm(){
+	let minimum_special = document.getElementById('minimum-special');
+	let same = document.getElementById('same-char');
+
 	const specialCharsRegex = /[^\w\s]/;
 	let password = document.accountForm.password.value;
 	let confirmPassword = document.accountForm.confirmPassword.value;
 
-	if (password.length < 8) {
-		openPopupBg();
-		minimum.classList.add("open-popup");
-		event.preventDefault();
-	}
-
-	if (!specialCharsRegex.test(password)) {
-		openPopupBg();
-		special.classList.add("open-popup");
+	if ((password.length < 8) || (!specialCharsRegex.test(password))) {
+		minimum_special.classList.remove("error-msg");
+		minimum_special.classList.add("error-p");
 		event.preventDefault();
 	}
 
 	if (password != confirmPassword) {
-		openPopupBg();
-		same.classList.add("open-popup");
+		same.classList.remove("error-msg");
+		same.classList.add("error-p");
 		event.preventDefault();
 	}
 
